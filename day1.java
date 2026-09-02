ea205498703aa3d0b6dc091dc692942bdfae2315
Fikri <fpiki5481@gmail.com>
2026-09-02 21:42:20 +0800

    Create day1.java
    ini adalah 3 bentuk umum komen di bahasa java

---
+++ day1.java
@@ -0,0 +1,14 @@
+public class day1 {
+    public static void main(String[] args) {
+
+        //komen biasa adalah komen satu baris
+        /*komen mullti line adalah komen yang lebih dari satu baris*/
+        /**komen javadoc adalah bagian kusus dari komen multi line */
+
+
+
+        
+
+
+    }
+}
