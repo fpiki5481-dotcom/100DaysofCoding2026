7698926d5aa5af64da1c02c4413df6383cfa4425
Fikri <fpiki5481@gmail.com>
2026-09-04 06:45:03 +0800

    Create day3.java
    fungsi dari 
    \n
    \t
    \"
    \\
    \f

---
+++ day3.java
@@ -0,0 +1,17 @@
+public class day3 {
+    public static void main(String[] args) {
+        System.out.println("Nama saya adalah MUH. FIKRI R" + "\n");
+
+        
+        System.out.println("Nama saya adalah  \nFIKRI");
+
+        System.out.println("Nama saya adalah  \tFIKRI ");
+
+        System.out.println("Nama saya adalah  \"FIKRI\" ");
+
+        System.out.println("Nama saya adalah  \\FIKRI ");
+
+        System.out.println("Nama saya adalah  \fFIKRI ");
+    }
+    
+}
