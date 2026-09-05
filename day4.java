0c319ffe012eb7f254aaad84e42301640b7a6d44
Fikri <fpiki5481@gmail.com>
2026-09-05 12:22:33 +0800

    Create day4.java
    nilai minimal sampai maksimal tipe data 
    byte
    short
    int
    long

---
+++ day4.java
@@ -0,0 +1,22 @@
+public class day4 {
+    public static void main(String[] args) {
+
+        
+
+        System.out.println(" ");
+
+        //angka minimal bilangan byte sampai maksimal
+        System.out.println(" Nilai byte dari bilangan "+ Byte.MIN_VALUE + " sampai " + Byte.MAX_VALUE +"\n");
+
+         //angka minimal bilangan short sampai maksimal 
+        System.out.println(" Nilai short dari bilangan "+ Short.MIN_VALUE + " sampai " + Short.MAX_VALUE +"\n");
+
+        //angka minimal bilangan int sampai maksimal 
+        System.out.println(" Nilai int dari bilangan "+ Integer.MIN_VALUE + " sampai " + Integer.MAX_VALUE +"\n");
+
+        //angka minimal bilangan long sampai maksimal 
+        System.out.println(" Nilai long dari bilangan "+ Long.MIN_VALUE + " sampai " + Long.MAX_VALUE +"\n");
+
+
+    }
+}
