0163fa63fc7805e1c4ca89a020c8c21ca6c35a52
Fikri <fpiki5481@gmail.com>
2026-09-06 13:23:51 +0800

    Create day5.java
    nilai minimal sampai maksimal tipe data float dan double serta contoh penggunaanya

---
+++ day5.java
@@ -0,0 +1,17 @@
+public class day5 {
+    public static void main(String[] args) {
+        
+        System.out.println(" bilangan float dari " + Float.MIN_VALUE + " sampai " + Float.MAX_VALUE +"\n");
+
+        System.out.println(" bilangan double dari " + Double.MIN_VALUE + " sampai " + Double.MAX_VALUE +"\n");
+
+        
+        //contoh pengguanaan bilangan float
+        float suhu = 36.5f;
+        double pi = 3.1415926535;
+
+        System.out.println("suhu ruangan b : " + suhu);
+        System.out.println("nilai pi adalah : " + pi);
+
+    }
+}
