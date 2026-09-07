04c5f718cae47603fbbe4581beee56038d450a44
Fikri <fpiki5481@gmail.com>
2026-09-07 08:47:25 +0800

    Create day6.java

---
+++ day6.java
@@ -0,0 +1,27 @@
+public class day6 {
+    public static void main(String[] args) {
+        
+        //tipe data char untuk memngimput 1 huruf atau tanda dan menggunakan tanda kutip 1
+        char F = 'a';
+
+        //tipe data boolean umtuk menentuan benar atau salah
+        boolean I = true;
+        boolean k = false;
+
+        
+        System.out.println("contoh tipe data char :"+ F);
+
+        System.out.println("contoh tipe data bolean :"+I);
+
+        System.out.println("contoh tipe data bolean :"+k);
+
+
+
+       
+
+           
+            
+        }
+
+    }
+
