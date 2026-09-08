2ff69fe649bb9d97405a6a775ed158dd73ea631f
Fikri <fpiki5481@gmail.com>
2026-09-08 08:59:18 +0800

    Create day7.java

---
+++ day7.java
@@ -0,0 +1,12 @@
+public class day7 {
+    public static void main(String[] args) {
+       
+        String nama,alamat;
+        nama = "fikri";
+        alamat = "GALUNG LOMBOK";
+
+        System.out.println("nama :"+ nama);
+        System.out.println("alamat :"+ alamat);
+
+    }
+}
