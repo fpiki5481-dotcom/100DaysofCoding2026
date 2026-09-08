07ff3e4355b44dbd50f93d202be11b88aaa67945
Fikri <fpiki5481@gmail.com>
2026-09-09 00:08:44 +0800

    Create day8.java

---
+++ day8.java
@@ -0,0 +1,18 @@
+public class day8 {
+    public static void main(String[] args) {
+        //deklarasi
+        String nama;
+
+      //inisialisasi
+        nama = "fikri";
+
+        System.out.println(" ");
+       System.out.printf("Nama saya sebelum diubah adalah : %s\n" ,nama );
+
+       //update variabel nama
+        nama = "hasriani";
+
+        System.out.printf("sesudah di ubah : %s \n\n",nama);
+
+    }
+}
