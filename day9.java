9464d9329da59b58bc48756374ce88e6bc2a9dbd
Fikri <fpiki5481@gmail.com>
2026-09-10 11:36:51 +0800

    Create day9.java

---
+++ day9.java
@@ -0,0 +1,14 @@
+public class day9 {
+    public static void main(String[] args) {
+        
+        //pengenalan tipe data final
+
+        final String nama = "fikri";
+        System.out.printf("nama saya adalah %s",nama);
+
+
+        //data string tidak dapat diubah, jika diubah akan eror karena data tersebut sudah final
+        
+        
+    }
+}
