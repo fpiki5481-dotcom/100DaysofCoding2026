0bac4a6644891bc1b63cf517e5ba702a3657a287
Fikri <fpiki5481@gmail.com>
2026-09-11 00:36:43 +0800

    Create day10.java

---
+++ day10.java
@@ -0,0 +1,18 @@
+public class day10 {
+    public static void main(String[] args) {
+       
+        System.out.println(" ");
+        System.out.println(" Nilai byte mulai dari bilangan "+ Byte.MIN_VALUE + " sampai " + Byte.MAX_VALUE +"\n");
+
+        System.out.println(" Nilai short mulai dari bilangan "+ Short.MIN_VALUE + " sampai " + Short.MAX_VALUE +"\n");
+
+        System.out.println(" Nilai int mulai dari bilangan "+ Integer.MIN_VALUE + " sampai " + Integer.MAX_VALUE +"\n");
+
+        System.out.println(" Nilai long mulai dari bilangan "+ Long.MIN_VALUE + " sampai " + Long.MAX_VALUE +"\n");
+
+        System.out.println(" Nlai double mulai dari bilangan "+ Double.MIN_VALUE + "sampai " + Double.MAX_VALUE +"\n");
+
+        System.out.println(" Nilai float mulai dari bilangan "+ Float.MIN_VALUE + "sampai" + Float.MAX_VALUE +"\n" );
+ 
+    }
+}
