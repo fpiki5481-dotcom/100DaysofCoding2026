54f5e0bec1fd78482377d879bd928e8cfad927c6
Fikri <fpiki5481@gmail.com>
2026-09-12 08:28:52 +0800

    Create day11.java

---
+++ day11.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class day11 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        
+        System.out.print("masukkan nama anda :");
+        String nama = in.nextLine();
+
+        System.out.print("maukkan umur anda :");
+        String alamat = in.nextLine();
+        
+        System.out.print("masukkan prodi anda :");
+        String prodi = in.nextLine();
+
+    }
+}
