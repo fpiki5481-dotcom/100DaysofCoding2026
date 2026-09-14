b12509d7710875ac2f92fc07f408f6697a926b5b
Fikri <fpiki5481@gmail.com>
2026-09-14 21:26:32 +0800

    Create day13.java

---
+++ day13.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class lat5 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int no = sc.nextInt();
+        int NO = sc.nextInt();
+        int Nomor = sc.nextInt();
+
+        System.out.printf("0%d-%d-%d",no,NO,Nomor);
+        
+
+
+    }
+}
