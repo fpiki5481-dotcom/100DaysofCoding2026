27735a79b0de6de3a811e237182b076127ed7ac1
Fikri <fpiki5481@gmail.com>
2026-09-15 18:33:59 +0800

    Create day14.java

---
+++ day14.java
@@ -0,0 +1,13 @@
+public class day14 {
+    public static void main(String[] args) {
+        // aritmatika penjulahan dan pengurangan 
+        
+       byte a = 2 + 4;
+       byte b = 8 - 4;
+
+       System.out.println("hasil dari 2 + 4 : " + a);
+       System.out.println("hasil dari 8 - 4 : " + b);
+
+
+    }
+}
