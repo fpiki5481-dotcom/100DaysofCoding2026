5fad9d0345e911a848b5137567309d044fb2e1ee
Fikri <fpiki5481@gmail.com>
2026-09-16 20:16:52 +0800

    Create day15.java

---
+++ day15.java
@@ -0,0 +1,12 @@
+public class day15 {
+    public static void main(String[] args) {
+        //aritmatika pembagian dan perkalian
+        
+        float a = 2 / 5f;
+        short b = 10 * 2;
+
+        System.out.println("hasil dari 2 / 5 : " + a);
+        System.out.println("hasil dari 10 * 2 : " +b);
+
+    }
+}
