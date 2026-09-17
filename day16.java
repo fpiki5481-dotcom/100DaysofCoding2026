077348853d01a7857a8e8b56a95a018282d24545
Fikri <fpiki5481@gmail.com>
2026-09-17 23:44:54 +0800

    Create day16.java

---
+++ day16.java
@@ -0,0 +1,12 @@
+public class day16 {
+    public static void main(String[] args) {
+        
+        //opertaor aritmatika modulus
+        byte a = 21;
+        byte b = 10;
+
+        int hasil = a % b;
+        System.out.println("hasil dari modulus dari 21 % 10 : " + hasil);
+
+    }
+}
