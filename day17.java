a91c704ff882cf19df59886f957dab145f56f00b
Fikri <fpiki5481@gmail.com>
2026-09-18 21:54:09 +0800

    Create day17.java

---
+++ day17.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+public class day17 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        //operator aritmatika penugasan
+        int angka = sc.nextInt();
+
+        angka += 5;
+        System.out.println(  angka);
+        angka -= 5;
+        System.out.println(  angka);
+        angka *= 5;
+        System.out.println(  angka);
+        angka /= 5;
+        System.out.println(  angka);
+        angka %= 5;
+        System.out.println(  angka);
+        
+        
+
+      
+      
+
+    }
+}
